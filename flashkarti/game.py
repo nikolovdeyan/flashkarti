60d2f3c4b62b1abeba89caff7e2f44aba55b4a33
Deyan Nikolov <deyan.nikolov@gmail.com>
2023-11-28 15:59:18 +0200

    Add Game and sample deck JSON

---
+++ flashkarti/game.py
@@ -0,0 +1,20 @@
+"""
+Stores the main program logic.
+""" 
+
+class Game:
+
+    def __init__(self):
+        self.deck = None
+        self.player = None
+        self.settings = None
+        self._gui = None
+
+    def select_deck(self):
+        pass
+
+    def select_player(self):
+        pass
+
+    def load_settings(self):
+        pass
