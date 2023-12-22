76ff8108b12d59708e34cbba59b2fc7547efb75a
Deyan Nikolov <deyan.nikolov@gmail.com>
2023-12-22 09:50:42 +0200

    Saving changes

--- src/deck.py
+++ src/deck.py
@@ -4,10 +4,25 @@ Represents a deck of cards, storing its state and possible actions.
 import random
 import logging
 
+from card import Card
+
 logger = logging.getLogger(__name__)
 
 
 class Deck:
+    """A Deck represents a collection of cards on a topic in the FlashKarti game.
+
+    The Deck stores information about the set of cards that are loaded in the current
+    quiz of a FlashKarti game. Each deck holds cards on a specific topic and a subset of
+    all cards can be selected to be used in the quiz. The Deck additionally supports
+    functionality to add, remove, and edit cards.
+
+    ### Attributes:
+    - `title (str)`: The title of the deck.
+    - `size (int)`: The number of cards in the deck.
+    - `quiz_size (int)`: The number of cards selected for the quiz.
+    """
+
     def __init__(self, title=None, cards=None):
         self.title = title
         self.size = 0
@@ -18,30 +33,27 @@ class Deck:
         self._num_answered_cards = 0
         self._update_size()
 
-    def add_card(self, card):
+    def add_card(self, card: Card) -> None:
+        """Adds a card to the deck.
+
+        ### Args:
+            `card (Card)`: The card to be added to the deck.
+        """
         self._cards.append(card)
         self._update_size()
 
-    def remove_card(self, card):
+    def remove_card(self, card: Card) -> None:
+        """Removes a provided card from the deck.
+
+        ### Args:
+            `card (Card)`: The card to be removed from the deck.
+        """
         for deck_card in self._cards:
             if deck_card.title == card.title:
                 self._cards.remove(deck_card)
                 self._update_size()
 
-    def _update_size(self):
-        self.size = len(self._cards)
-
-    def _update_quiz_size(self):
-        self.quiz_size = len(self._quiz_cards)
-
-    def _update_num_answered_cards(self):
-        result = 0
-        for card in self._cards:
-            if card.user_answer:
-                result += 1
-        self._num_answered_cards = result
-
-    def draw_quiz_cards(self, num_cards):
+    def draw_quiz_cards(self, num_cards: int) -> None:
         self._quiz_cards = random.sample(self._cards, num_cards)
         self._update_quiz_size()
 
@@ -68,6 +80,19 @@ class Deck:
 
     def shuffle_deck(self):
         random.shuffle(self._cards)
+
+    def _update_size(self):
+        self.size = len(self._cards)
+
+    def _update_quiz_size(self):
+        self.quiz_size = len(self._quiz_cards)
+
+    def _update_num_answered_cards(self):
+        result = 0
+        for card in self._cards:
+            if card.user_answer:
+                result += 1
+        self._num_answered_cards = result
 
     def __repr__(self):
         return f"Deck: {self.title}: {[card for card in self._cards]}"
---
+++ tests/test_deck.py
@@ -0,0 +1,63 @@
+import pytest
+
+from src.deck import Deck
+
+
+@pytest.fixture
+def fx_cards(mocker):
+    fake_card1 = mocker.patch("src.card.Card")
+    fake_card1.title = "Fake Card 1"
+    fake_card1.contents = "Fake Contents 1"
+    fake_card1.answer = "Fake Answer 1"
+    fake_card1.references = "Fake References 1"
+    fake_card2 = mocker.patch("src.card.Card")
+    fake_card2.title = "Fake Card 2"
+    fake_card2.contents = "Fake Contents 2"
+    fake_card2.answer = "Fake Answer 2"
+    fake_card2.references = "Fake References 2"
+
+    fake_cards = [fake_card1, fake_card2]
+
+    yield fake_cards
+
+
+def test__add_card__with_card_added__appends_card(fx_cards):
+    deck = Deck()
+    fake_card = fx_cards[0]
+
+    deck.add_card(fake_card)
+
+    assert fake_card in deck._cards
+
+
+def test__add_card__with_card_added__updates_size(fx_cards):
+    deck = Deck()
+    fake_card = fx_cards[0]
+
+    assert deck.size == 0
+
+    deck.add_card(fake_card)
+
+    assert deck.size == 1
+
+
+def test__remove_card__with_existing_card__removes_card(fx_cards):
+    deck = Deck()
+    fake_card = fx_cards[0]
+    deck._cards.append(fake_card)
+    deck.size = 1
+
+    deck.remove_card(fake_card)
+
+    assert fake_card not in deck._cards
+
+
+def test__remove_card__with_existing_card__updates_size(fx_cards):
+    deck = Deck()
+    fake_card = fx_cards[0]
+    deck._cards.append(fake_card)
+    deck.size = 1
+
+    deck.remove_card(fake_card)
+
+    assert deck.size == 0
