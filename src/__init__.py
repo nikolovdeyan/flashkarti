0d396b91541f568c11357c9437a047879b8c2d06
Deyan Nikolov <deyan.nikolov@gmail.com>
2024-01-04 17:39:04 +0200

    Add demo deck

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Deyan Nikolov"
+__copyright__ = "Copyright 2024"
+__credits__ = []
+__license__ = "GPL (GNU General Public License)"
+__version__ = "1.0"
+__maintainer__ = "Deyan Nikolov"
+__email__ = ""
+__status__ = "Prototype"
